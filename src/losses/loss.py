2a6e4818b2fc89a5fd7e4b3c710ca65fdc1b331d
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-12-27 12:11:59 +0900

    Update loss.py

--- src/losses/loss.py
+++ src/losses/loss.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
 
 
 class Total_loss(nn.Module):
@@ -11,10 +10,7 @@ class Total_loss(nn.Module):
         self.opt = loss_opt
 
     def forward(self, logits, target):
-        upsampled_logits = nn.functional.interpolate(
-            logits, size=target.shape[-2:], mode="bilinear", align_corners=False
-        )
-        ce_loss = self.ce(upsampled_logits, target.type(torch.long))
+        ce_loss = self.ce(logits, target.type(torch.long))
 
         ce_loss *= self.opt.ce_coef
         loss = ce_loss
